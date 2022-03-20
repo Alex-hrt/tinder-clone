8cfefa8ed4bb8ba15699c20f0bba10ab231ae7b1
Alex Héritier <alex.heritier@outlook.fr>
2022-03-20 18:29:14 +0100

    added app store logos

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,3 +1,7 @@
+import AppleStoreLogo from "../images/apple-app-store.webp";
+import GoogleStoreLogo from "../images/google-play-store.webp";
+import TinderLogoSmall from "../images/tinder-logo-small.png";
+
 import { useState } from "react";
 
 const AuthModal = ({ setShowModal, isSignUp }) => {
@@ -29,6 +33,7 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
     return (
         <div className="auth-modal">
             <div onClick={handleClick}><span className="close-icon">&#11198;</span></div>
+            <img className="tinder-logo-small" src={TinderLogoSmall}></img>
             <h2>{isSignUp ? "CREATE ACCOUNT" : "LOG IN"}</h2>
             <p>By clicking Log in, you agree to our <a href="https://policies.tinder.com/terms?lang=en" target="_blank">Terms</a>. Learn how we process your data in our <a href="https://policies.tinder.com/privacy?lang=en" target="_blank">Privacy Policy</a> and <a href="https://policies.tinder.com/cookie-policy?lang=en" target="_blank">Cookie Policy</a>.</p>
             <form onSubmit={handleSubmit}>
@@ -61,11 +66,10 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
             </form>
             <hr/>
             <h2>GET THE APP!</h2>
-            
-            
-            
-            
-            
+            <div className="phone-app-store">
+                <a href="https://apps.apple.com/fr/app/tinder/id547702041" target="_blank"><img className="apple-store-logo" src={AppleStoreLogo}/></a>
+                <a href="https://play.google.com/store/apps/details?id=com.tinder&hl=fr&gl=US" target="_blank"><img className="google-play-logo" src={GoogleStoreLogo}/></a>
+            </div>
         </div>
     )
 }
