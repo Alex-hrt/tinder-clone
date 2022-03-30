be8588a794ccd919b382706761e036fedcc27a81
Alex Héritier <alex.heritier@outlook.fr>
2022-03-30 14:29:30 +0200

    formatted code

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -4,21 +4,32 @@ import MatchesDisplay from "./MatchesDisplay";
 import { useState } from "react";
 
 const ChatContainer = ({ user }) => {
-    const [ clickedUser, setClickedUser ] = useState(null)
+	const [clickedUser, setClickedUser] = useState(null);
 
-    return (
-        <div className="chat-container">
-            <ChatHeader user={user} />
+	return (
+		<div className="chat-container">
+			<ChatHeader user={user} />
 
-            <div>
-                <button className="option" onClick={() => setClickedUser(null)} >Matches</button>
-                <button className="option" disabled={!clickedUser}>Messages</button>
-            </div>
+			<div>
+				<button className="option" onClick={() => setClickedUser(null)}>
+					Matches
+				</button>
+				<button className="option" disabled={!clickedUser}>
+					Messages
+				</button>
+			</div>
 
-            {!clickedUser && <MatchesDisplay matches={user.matches} setClickedUser={setClickedUser}/>}
-            {clickedUser && <ChatDisplay user={user} clickedUser={clickedUser} />}
-        </div>
-    )
-}
+			{!clickedUser && (
+				<MatchesDisplay
+					matches={user.matches}
+					setClickedUser={setClickedUser}
+				/>
+			)}
+			{clickedUser && (
+				<ChatDisplay user={user} clickedUser={clickedUser} />
+			)}
+		</div>
+	);
+};
 
-export default ChatContainer
+export default ChatContainer;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,49 +1,52 @@
 import Nav from "../components/Nav";
 import { useState } from "react";
-import AuthModal from '../components/AuthModal';
-import { useCookies } from "react-cookie"
+import AuthModal from "../components/AuthModal";
+import { useCookies } from "react-cookie";
 
 const Home = () => {
+	const [showModal, setShowModal] = useState(false);
+	const [isSignUp, setIsSignUp] = useState(true);
+	const [cookies, setCookie, removeCookie] = useCookies(["user"]);
 
-    const [showModal, setShowModal] = useState(false);
-    const [isSignUp, setIsSignUp] = useState(true);
-    const [ cookies, setCookie, removeCookie ] = useCookies(["user"])
+	const authToken = cookies.authToken;
 
-    const authToken = cookies.authToken;
+	const handleClick = () => {
+		if (authToken) {
+			removeCookie("UserId", cookies.UserId);
+			removeCookie("AuthToken", cookies.authToken);
+			window.location.reload();
+			return;
+		}
 
-    const handleClick = () => {
-        if (authToken) {
-            removeCookie("UserId", cookies.UserId)
-            removeCookie("AuthToken", cookies.authToken)
-            window.location.reload()
-            return
-        }
+		setShowModal(true);
+		setIsSignUp(true);
+	};
 
-        setShowModal(true);
-        setIsSignUp(true);
-    }
+	return (
+		<div className="overlay">
+			<Nav
+				authToken={authToken}
+				minimal={false}
+				setShowModal={setShowModal}
+				showModal={showModal}
+				setIsSignUp={setIsSignUp}
+			/>
+			<div className="home">
+				<h1 className="primary-title">
+					Swipe Right<span>&#174;</span>
+				</h1>
+				<button className="primary-button" onClick={handleClick}>
+					{authToken ? "Signout" : "Create Account"}
+				</button>
 
-    return (
-        <div className="overlay">
-            <Nav 
-                authToken={authToken}
-                minimal={false}
-                setShowModal={setShowModal}
-                showModal={showModal}
-                setIsSignUp={setIsSignUp}
-            />
-            <div className="home">
-                <h1 className="primary-title">Swipe Right<span>&#174;</span></h1>
-                <button className="primary-button" onClick={handleClick}>
-                    {authToken ? "Signout" : "Create Account"}
-                </button>
-
-                {showModal && (
-                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp}/>
-                )}
-
-            </div>
-        </div>
-    )
-}
-export default Home
+				{showModal && (
+					<AuthModal
+						setShowModal={setShowModal}
+						isSignUp={isSignUp}
+					/>
+				)}
+			</div>
+		</div>
+	);
+};
+export default Home;
